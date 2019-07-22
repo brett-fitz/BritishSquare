b2ca4fa0cee4bf217304d2ac19c5bc1663fd3ce2
Brett Fitzpatrick <brett@Bretts-MacBook-Pro-2.local>
2019-07-22 06:02:12 -0400

    Documentation

---
+++ BritishSquare.asm
@@ -0,0 +1,15 @@
+# File:         build.asm
+# Author:       Brett Fitzpatrick
+# Section:	01
+#
+# Description:  This file implements the 2-player game British Square. 
+#		The game entails two players alternating turns to drop 
+# 		stones on the board. The overall objective is to have 
+#		the most stones in the game. The game ends when neither
+#		player can make a legal drop. A drop is only legal if 
+#		there are no enemy stones orthogonally adjacent to the
+#		desired empty cell. 
+#
+#
+
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BritishSquare
-An implementation of the British Square Game done in assembly. 
+The game  British Square implemented in MIPS assembly.
+
+Complete set of rules: http://www.di.fc.ul.pt/~jpn/gv/britsquare.htm
+
+Note: This implementation differs slightly then what is described on the above site. # 0-24 board && only one round
